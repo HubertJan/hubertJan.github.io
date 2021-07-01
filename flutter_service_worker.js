6bf26b5187443290c7944b5671f17691ad81f5b8
HubertJan <70658889+HubertJan@users.noreply.github.com>
2021-07-02 00:40:11 +0200

    build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "8fe0383ed33812d690dfd89e6efec8e7",
-"/": "8fe0383ed33812d690dfd89e6efec8e7",
-"main.dart.js": "7eecd47a1ee2bfacc6db122cd41a198c",
+"index.html": "d2511bf47f4f341713ae6c3c3e7a968e",
+"/": "d2511bf47f4f341713ae6c3c3e7a968e",
+"main.dart.js": "c7b3905ecb57e46213da34932b5050da",
 "manifest.json": "f1b1f5543481fe36f3324dbe5ed09a89",
 "version.json": "4b36d4cce07e228c6c062fec32cf689e"
 };
